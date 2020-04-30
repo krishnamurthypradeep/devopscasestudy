b1c073a51a7621f6db9024923077e3765157fead
pradeep <pradeeepmurthy@gmail.com>
2020-04-30 15:23:06 +0530

    Updated scm and artifactory

---
+++ src/main/java/com/example/devopscasestudy/model/Products.java
@@ -0,0 +1,21 @@
+package com.example.devopscasestudy.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Products {
+	
+private List<Product> productList;
+    
+    public List<Product> getProductList() {
+        if(productList == null) {
+        	productList = new ArrayList<>();
+        }
+        return productList;
+    }
+ 
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
+
+}
