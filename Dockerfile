e081951833c4e2a6ded5a58a7d88300d1f25269e
pradeep <pradeeepmurthy@gmail.com>
2020-05-20 15:58:50 +0530

    prepared the contents for docker image with jenkins pipeline

--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,4 @@
-
-
-
-
-FROM maven:3.5.2-jdk-8-alpine AS maven_build
-
-COPY pom.xml /tmp/
-
-COPY src /tmp/src/
-
-WORKDIR /tmp/
-
-RUN mvn package
-
-#pull base image
-
-FROM openjdk:8-jdk-alpine
-
-#maintainer 
-MAINTAINER pradeep@gmail.com
-#expose port 8080
-EXPOSE 8888
-
-#default command
-CMD java -jar /home/devopscasestudy-0.4.0-SNAPSHOT.war
-
-#copy hello world to docker image from builder image
-
-COPY --from=maven_build /tmp/target/devopscasestudy-0.4.0-SNAPSHOT.war /home/devopscasestudy-0.4.0-SNAPSHOT.war
+FROM openjdk:8
+ADD target/devopscasestudy-docker.war devopscasestudy-docker.war
+EXPOSE 8083
+ENTRYPOINT ["java", "-jar", "devopscasestudy-docker.war"]
---
+++ src/main/java/com/example/devopscasestudy/api/WebController.java
@@ -0,0 +1,36 @@
+package com.example.devopscasestudy.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+ 
+@Controller
+public class WebController {
+	
+	
+	// INSERT INTO users  VALUES ('satish', ENCRYPT('pass1'));
+   
+    @RequestMapping(value="/")
+    public String home(){
+        return "home";
+    }
+   
+    @RequestMapping(value="/user")
+    public String user(){
+        return "user";
+    }
+  
+    @RequestMapping(value="/admin")
+    public String admin(){
+        return "admin";
+    }
+   
+    @RequestMapping(value="/login")
+    public String login(){
+        return "login";
+    }
+   
+    @RequestMapping(value="/403")
+    public String Error403(){
+        return "403";
+    }
+}
